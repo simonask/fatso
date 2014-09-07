2d3650ad92d5cd65ced8e1d5c736c9cc1d21e145
Simon Ask Ulsnes <simon@ulsnes.dk>
2014-09-07 11:19:36 +0200

    run fatso_update_packages prior to generating dependency graph

--- install.c
+++ install.c
@@ -9,6 +9,9 @@ fatso_install(struct fatso* f, int argc, char* const* argv) {
   r = fatso_load_project(f);
   if (r != 0) goto out;
 
+  r = fatso_update_packages(f);
+  if (r != 0) goto out;
+
   r = fatso_load_or_generate_dependency_graph(f);
   if (r != 0) goto out;
 
