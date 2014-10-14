07d902aa61bfcfe078d3ce1f15fee2faf465719f
Simon Ask Ulsnes <simon@ulsnes.dk>
2014-10-14 12:39:23 +0200

    fatso exec: load project if possible (but don't error if it fails)

--- exec.c
+++ exec.c
@@ -8,6 +8,16 @@ int
 fatso_exec(struct fatso* f, int argc, char* const* argv) {
   int r;
 
+  r = fatso_load_project(f);
+  if (r != 0) {
+    fatso_logf(f, FATSO_LOG_WARN, "Warning: No project loaded.");
+  } else {
+    r = fatso_load_or_generate_dependency_graph(f);
+    if (r != 0) {
+      fatso_logf(f, FATSO_LOG_WARN, "Warning: Error generating dependency graph.");
+    }
+  }
+
   r = fatso_load_environment(f);
   if (r != 0) {
     goto out;
